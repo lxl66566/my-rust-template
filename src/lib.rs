b4b173da55f14f799f4c830cd8b5d7de930d825c
lxl66566 <lxl66566@gmail.com>
2026-06-30 18:50:50 +0800

    init
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(clippy::pedantic, clippy::cargo)]
+#![allow(
+    clippy::missing_panics_doc,
+    clippy::missing_errors_doc,
+    clippy::doc_markdown
+)]
+
+pub fn test() {
+    println!("Hello, world!");
+}
